906043a4bec8ce3231918369b723563e14b35b3c
santiagomanso <santiagomanso@web.de>
2022-07-05 19:08:13 +0200

    🔨 created routes - Nav

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Nav from './components/Nav'
+import { ModalProvider } from './context/ModalContext'
+
+import HomeScreen from './screens/HomeScreen'
+import PortfolioScreen from './screens/PortfolioScreen'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='w-screen h-screen flex justify-center  app'>
+      <ModalProvider>
+        <BrowserRouter>
+          <Nav />
+          <Routes>
+            <Route exact path='/' element={<HomeScreen />} />
+            <Route path='/portfolio' element={<PortfolioScreen />} />
+          </Routes>
+        </BrowserRouter>
+      </ModalProvider>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
