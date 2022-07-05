58c9832ba9058c7e42f16d4389240433f91cccb4
santiagomanso <santiagomanso@web.de>
2022-07-05 19:02:18 +0200

    🔨 Tailwind config init / colors - animation added

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#17181B',
+        secondary: '#94A4B4',
+        variant1: '#343A41',
+        variant2: '#22262A',
+        github: '#22272E',
+      },
+      fontFamily: {
+        console: 'Inconsolata',
+      },
+      animation: {
+        'spin-slow': 'spin 10s linear infinite',
+      },
+    },
+  },
+  plugins: [],
+}
