ab7d06fde95c2fd88a4745f2780b21b77c131f4a
santiagomanso <santiagomanso@web.de>
2023-04-02 19:13:13 +0200

    🔧 abstracted features into separate files

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -47,41 +47,6 @@ const Header = ({ title, subtitle, typeWriterText }: headerInterface) => {
               multiTextLoop
             />
           </div>
-          <div className='hidden lg:flex flex-col items-center pr-[0.20rem] '>
-            <a
-              href='https://www.linkedin.com/in/santimanso/'
-              target='_blank'
-              rel='noreferrer'
-              className=' flex items-baseline opacity-30 hover:opacity-100 gap-1 text-2xl group'
-            >
-              <label className='cursor-pointer scale-0 group-hover:scale-100 transition-all duration-[400ms] translate-x-14 group-hover:-translate-x-2 '>
-                linked
-              </label>
-              <i className='cursor-pointer  text-white group-hover:text-[#0A66C2]  fa-brands fa-linkedin-in text-3xl group-hover:-rotate-[20deg] group-hover:scale-[1.5] transition-all ease-in duration-300 '></i>
-            </a>
-            <a
-              href='https://github.com/santiagomanso'
-              target='_blank'
-              rel='noreferrer'
-              className='flex items-center opacity-30 hover:opacity-100 gap-1 text-2xl group'
-            >
-              <label className='cursor-pointer scale-0 group-hover:scale-100 transition-all duration-[400ms] translate-x-14 group-hover:-translate-x-2 '>
-                github
-              </label>
-              <i className='cursor-pointer text-white group-hover:text-[#fafafa]  fa-brands fa-github text-3xl group-hover:-rotate-[20deg] group-hover:scale-[1.5] transition-all ease-in duration-300'></i>
-            </a>
-          </div>
-        </div>
-      </div>
-      <div className='flex flex-col md:flex-row lg:items-center justify-between gap-3  text-4xl select-none '>
-        <div
-          onClick={handlerNavigate}
-          className='flex justify-center mt-5 md:mt-0 items-center gap-3 group  transition-all ease-in-out duration-300 hover:cursor-pointer cta__home'
-        >
-          <span className='text-xl lg:text-3xl bg-purple-500/80 text-white/60 group-hover:text-white tracking-wide scale-100 group-hover:-translate-x-6 group-hover:scale-[1.2] group-hover:-translate-y-2 transition-all ease-in-out duration-300   px-8 py-2 rounded-lg'>
-            my work
-          </span>
-          <i className='hidden md:inline fa-solid fa-display text-3xl text-white/60 group-hover:text-purple-500/80 group group-hover:rotate-[20deg] group-hover:scale-[1.5] transition-all ease-in duration-300 '></i>
         </div>
       </div>
     </section>
