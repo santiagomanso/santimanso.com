9297e0f6b41708fe896f975f11cd99b465ca8a39
santiagomanso <santiagomanso@web.de>
2023-04-03 12:26:18 +0200

    🎨 increased width of app

--- src/components/containers/AppContainer.tsx
+++ src/components/containers/AppContainer.tsx
@@ -2,8 +2,8 @@ import { WithProps } from '../../types/types'
 
 const AppContainer = ({ children }: WithProps) => {
   return (
-    <div className='h-screen bg-gradient-to-bl from-neutral-800 to-zinc-800 flex justify-center items-center '>
-      <main className='flex flex-col  h-full max-w-7xl w-full lg:py-8'>
+    <div className='h-screen bg-gradient-to-bl from-neutral-800 to-zinc-800 flex justify-center items-center overflow-y-auto overflow-x-hidden'>
+      <main className='flex flex-col h-full max-w-[85rem] lg:justify-center w-full lg:py-5'>
         {children}
       </main>
     </div>
