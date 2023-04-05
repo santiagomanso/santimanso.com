c5cfb9e8f98245eaf63a27c7fe059668930f73a0
santiagomanso <santiagomanso@web.de>
2023-04-05 23:44:42 +0200

    🔨 created component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className='fixed w-full bottom-5 left-0 text-secondary flex items-center justify-center'>
+      <span className='text-3xl'>@Santiagomanso</span>
+    </footer>
+  )
+}
+
+export default Footer
