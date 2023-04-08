bab73e7a37c46584de46281694ec6ba26748d028
santiagomanso <santiagomanso@web.de>
2023-04-08 20:16:16 +0200

    🎨 reworked css

--- src/components/containers/MainContainer.tsx
+++ src/components/containers/MainContainer.tsx
@@ -15,7 +15,7 @@ const MainContainer = ({
       className={` ${animationFade} bg-gradient-to-tl
        from-gray-200 via-gray-300 to-gray-500  lg:rounded-sm  transition-colors ease-in-out duration-500  dark:from-zinc-900/70 dark:to-neutral-700 flex flex-col   h-full lg:h-3/4 lg:p-0 $ overflow-auto items-center
        ${padding ? padding : 'px-5 py-4'}
-      ${border ? 'border border-black dark:border-neutral-900' : ''}
+      ${border ? 'border border-neutral-900 dark:border-neutral-700' : ''}
       ${gap ? gap : 'gap-10'}
       ${justifyCenter ? ' justify-center' : 'justify-start'}
       `}
