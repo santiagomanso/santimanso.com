71f64d9f04bcb771572dec45aa59c0da23003e1f
santiagomanso <santiagomanso@web.de>
2023-04-09 15:14:56 +0200

    🔧 reworked component, implemented interface Props

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,24 @@
-const Footer = () => {
+//one footer but showed from the <Navbar /> inside of responsive menu.. and also on <App />
+interface FooterPropsI {
+  displayOnPc?: boolean
+  displayOnPhones?: boolean
+}
+
+const Footer = ({ displayOnPc, displayOnPhones }: FooterPropsI) => {
   return (
-    <footer className='fixed w-full bottom-5 left-0 text-secondary flex items-center justify-center'>
-      <span className='text-3xl'>@Santiagomanso</span>
+    <footer
+      className={`fixed w-full bottom-5 left-0 text-secondary  items-center justify-center
+      ${displayOnPc ? 'hidden lg:flex' : ''}
+      ${displayOnPhones ? 'flex lg:hidden' : ''}`}
+    >
+      <a
+        href='https://github.com/santiagomanso'
+        target='_blank'
+        rel='noreferrer'
+        className='text-3xl'
+      >
+        @Santiagomanso
+      </a>
     </footer>
   )
 }
