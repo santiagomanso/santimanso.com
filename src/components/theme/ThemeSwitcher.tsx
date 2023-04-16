c9198070a6314bbdc2b3bcb6c71e758a6946d940
santiagomanso <santiagomanso@web.de>
2023-04-16 11:26:42 +0200

    🔧 reworked component css

--- src/components/theme/ThemeSwitcher.tsx
+++ src/components/theme/ThemeSwitcher.tsx
@@ -18,19 +18,17 @@ const ThemeSwitcher = ({ setOpen }: ThemeSwitcherProps) => {
   return (
     <div
       onClick={handleSelect}
-      className='flex w-20 h-10 bg-gray-200 dark:bg-purple-700 outline outline-1 transition-colors cursor-pointer rounded-full'
+      className='flex transition-colors cursor-pointer rounded-full'
     >
       <div
         className={`${
-          isSelected === 'dark'
-            ? 'translate-x-full text-purple-600'
-            : 'translate-x-0 text-yellow-600'
-        } transition-all ease-in-out duration-300 w-10 bg-gray-200 border border-gray-700 dark:border-secondary dark:bg-gray-800 rounded-full flex justify-center items-center`}
+          isSelected === 'dark' ? 'translate-x-full ' : 'translate-x-0 '
+        } transition-all ease-in-out duration-300 flex justify-center items-center`}
       >
         <i
           className={`${
             isSelected === 'dark' ? 'fa-solid fa-moon' : 'fa-solid fa-sun'
-          } text-2xl`}
+          }  text-xl`}
         ></i>
       </div>
     </div>
