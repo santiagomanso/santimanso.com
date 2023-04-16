4faef0a214cdd26576733bdac901095d40dfa294
santiagomanso <santiagomanso@web.de>
2023-04-16 11:29:51 +0200

    🚑 fixed problem with index to change text

--- src/interfaces/navItemsInterface.ts
+++ src/interfaces/navItemsInterface.ts
@@ -2,7 +2,12 @@ export interface navItems {
   id: number
   icon: string //fontawesome icon
   path: string
-  text: string
+  text: {
+    [index: string]: string
+    en: string
+    es: string
+    de: string
+  }
   arrow: string
   newTab: Boolean
 }
