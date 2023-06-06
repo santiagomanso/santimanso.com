8ed55b2abab0665b2b13150455dcb24194792e66
santiagomanso <santiagomanso@web.de>
2023-06-06 16:39:00 +0200

    📝 added challenges text

--- src/data/diccionary.ts
+++ src/data/diccionary.ts
@@ -10,6 +10,7 @@ export const diccionary: Dictionary = {
     aboutMe: 'about me',
     aboutMeText:
       "I'm a full stack developer based in Berlin, I attended a full time (800+ hours) bootcamp (web development with Typescript+NextJs). At the academy I was happy to be around other people who were venturing into the world of coding. I have been developing applications with React, JavaScript, TypeScript and different CSS frameworks for more than 2 years. Being Tailwind Css the one I like the most.",
+    challanges: 'Development Challenges',
   },
   es: {
     myWork: 'mis trabajos',
@@ -20,6 +21,7 @@ export const diccionary: Dictionary = {
     aboutMe: 'Quien soy',
     aboutMeText:
       'Soy un desarrollador full-stack con sede en Berlín, asistí a un tiempo completo (800 + horas) bootcamp (desarrollo web con Typescript + NextJs). En la academia estaba feliz de estar rodeado de otras personas que se aventuraban en el mundo de la codificación. Llevo más de 2 años desarrollando aplicaciones con React, JavaScript, TypeScript y diferentes frameworks CSS. Siendo Tailwind Css el que más me gusta.',
+    challanges: 'Desafios Superados',
   },
   de: {
     myWork: 'meine projekte',
@@ -30,5 +32,6 @@ export const diccionary: Dictionary = {
     aboutMe: 'über mich',
     aboutMeText:
       'Als Full-Stack-Entwickler arbeite ich in Berlin und habe vor einiger Zeit an einem intensiven Vollzeit-Bootcamp teilgenommen, welches sich auf Webentwicklung mit Typescript+NextJs spezialisiert hatte. Das Bootcamp umfasste insgesamt mehr als 800 Stunden und ermöglichte es mir, mich mit anderen Menschen zu umgeben, die ebenfalls ihr Interesse an der Programmierung entdeckt hatten. Seit mehr als zwei Jahren entwickle ich nun Anwendungen mit React, JavaScript, TypeScript und verschiedenen CSS-Frameworks. Dabei hat sich Tailwind CSS als mein bevorzugtes Framework herauskristallisiert.erweitere.',
+    challanges: 'Herausforderungen',
   },
 }
